2e6f81bc8e8570b7dd9b3e1342703b8d3ab68ec1
DHANUSH MANICKAM <139457808+dhanush-manickam@users.noreply.github.com>
2024-12-31 20:33:03 +0530

    Update Farey algorithm.py

--- Farey algorithm.py
+++ Farey algorithm.py
@@ -1,29 +1,31 @@
-# farey"s algorithm to approximate a decimal number
+# Initialize starting fractions
+# a/b represents the first fraction (0/1)
+# c/d represents the second fraction (1/1)
+a, b, c, d = 0, 1, 1, 1
 
-# Initializing some variables
-# a,b is the first fraction 0/1
-# c,d is the second fraction 1/1
-a,b,c,d = 0,1,1,1
-n = float(input("enter your decimal number between 0-1: "))
-precision = len(str(n)) - 2
+# Take user input for the decimal number to approximate
+n = float(input("Enter a decimal number between 0 and 1: "))
 
-# loop that calculate the fraction
-while round((a/b),precision)!=round(n,precision) or round((c/d),precision)!=round(n,precision):
-	# this is the median part
-	# e/f is the median of a/b and c/d
-	# e/f is always less than c/d 
-	e = a + c
-	f = b + d
-	# if the input decimal is less than e/f the new c/d
-	if n < e/f:
-		c,d = e,f
-	# or else we want to set e/f as the new a/b
-	else:
-		a,b = e,f
-		
+# Calculate the required precision based on the length of the decimal input
+precision = len(str(n)) - 2  # Subtract 2 to exclude '0.'
 
-# This block of code is specifically to print the fractional approximation of the decimal
-if a/b == round(n,precision):
-	print(f'This is the fractional approximation of the decimal {a}/{b}')
+# Loop to calculate the fraction that approximates the given decimal
+while round((a / b), precision) != round(n, precision) or round((c / d), precision) != round(n, precision):
+    # Compute the mediant (e/f) of the fractions a/b and c/d
+    e = a + c  # Numerator of the mediant
+    f = b + d  # Denominator of the mediant
+
+    # Adjust the fractions based on the input value
+    # If n is less than e/f, update c/d to e/f
+    if n < e / f:
+        c, d = e, f
+    # Otherwise, update a/b to e/f
+    else:
+        a, b = e, f
+
+# Check if the final approximation matches the input value
+# Print the fractional approximation
+if a / b == round(n, precision):
+    print(f'The fractional approximation of the decimal is {a}/{b}')
 else:
-	print(f'This is the fractional approximation of the decimal {c}/{d}')
+    print(f'The fractional approximation of the decimal is {c}/{d}')
